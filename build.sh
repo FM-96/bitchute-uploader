1d86dc3930f3b80a62a6b7e292fa15f567b9d24a
Felix Müllner <felix.muellner.96@gmail.com>
2019-11-20 15:31:29 +0100

    Add version to build file names

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gox -ldflags="-X main.version=$(git describe --always --dirty)" -output="$GOPATH/out/{{.Dir}}/$(git describe --always --dirty)/{{.Dir}}_{{.OS}}_{{.Arch}}"
+gox -ldflags="-X main.version=$(git describe --always --dirty)" -output="$GOPATH/out/{{.Dir}}/$(git describe --always --dirty)/{{.Dir}}_$(git describe --always --dirty)_{{.OS}}_{{.Arch}}"
